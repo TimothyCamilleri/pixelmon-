fa67fd167c31b51d6e25de62aa02897d8e2afdcc
Timothy Camilleri <timothycamilleri@hotmail.com>
2022-04-08 23:25:14 +0100

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-DATA_DIR=/opt/pixelmon/pixelmon
-MINECRAFT_DIR=/opt/pixelmon/minecraft
-
-docker run --name=pixelmon-container --rm -it -p 25565:25565 \
--v $DATA_DIR/ops.json:$MINECRAFT_DIR/ops.json \
--v $DATA_DIR/server.properties:$MINECRAFT_DIR/server.properties \
--v $DATA_DIR/config:$MINECRAFT_DIR/config \
--v $DATA_DIR/world:$MINECRAFT_DIR/world \
-walthowd_warrush/pixelmon
